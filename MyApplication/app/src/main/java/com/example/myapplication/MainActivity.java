8ef2d82b4507c5b8522b83fe6e94f521678f3840
Emannuel Oliveira Pedreira <emannuelop22@gmail.com>
2024-09-14 08:09:31 -0300

    projeto

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView nameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameView = findViewById(R.id.textView);
+        nameView.setText("Olá mundo!");
+    }
+
+    public void tela2(View view) {
+        Intent it = new Intent(getApplicationContext(), MainActivity2.class);
+        startActivity(it);
+    }
+}
